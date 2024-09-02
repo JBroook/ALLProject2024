c735d6af99b3619b2e3dfd698a42ec7fc6dea78a
JBroook <JeremiahBoey@gmail.com>
2024-09-02 11:36:18 +0800

    Window will open to full screen

---
+++ ALLProject/main/Main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Car Rental App")
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry("%dx%d" % (width, height))
+root.state("zoomed")
+
+
+root.mainloop()
