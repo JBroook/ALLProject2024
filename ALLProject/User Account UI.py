37f67f0873f152f5c78df3a6c346ef2a3f511596
GoonHee <ghwwong@gmail.com>
2024-10-18 08:00:43 +0800

    Update User Account UI.py

--- ALLProject/User Account UI.py
+++ ALLProject/User Account UI.py
@@ -3,9 +3,9 @@ from customtkinter import CTkImage
 from PIL import Image
 
 root=customtkinter.CTk()
-root.geometry("1707x1000")
+root.geometry("1200x800")
 
-user_account_ui=customtkinter.CTkImage(light_image=Image.open("Project Images/DriveEase UI.png"),size=(1707,1000))
+user_account_ui=customtkinter.CTkImage(light_image=Image.open("Project Images/DriveEase UI.png"),size=(root.winfo_screenwidth(),root.winfo_screenheight()-68))
 user_account_ui_label=customtkinter.CTkLabel(root,image=user_account_ui,text="")
 user_account_ui_label.place(x=0,y=0,anchor="nw")
 
@@ -48,5 +48,8 @@ email_entry.place(x=704.58/1707*root.winfo_screenwidth(),y=638.95/1067*root.winf
 
 back_btn=customtkinter.CTkButton(root,width=176.7/1707*root.winfo_screenwidth(),height=75/1067*root.winfo_screenheight(),bg_color="white",fg_color="#1572D3",text="Back",text_color="white",font=("Poppins Light",24))
 back_btn.place(x=81.13/1707*root.winfo_screenwidth(),y=891.25/1067*root.winfo_screenheight(),anchor="nw")
+
+edit_btn=customtkinter.CTkButton(root,width=176.7/1707*root.winfo_screenwidth(),height=75/1067*root.winfo_screenheight(),bg_color="white",fg_color="#1572D3",text="Edit",text_color="white",font=("Poppins Light",24))
+edit_btn.place(x=1350/1707*root.winfo_screenwidth(),y=720/1067*root.winfo_screenheight(),anchor="nw")
 
 root.mainloop()
